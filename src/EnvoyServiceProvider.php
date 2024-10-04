28b60c102e486148c84690d7a1eb31490d5b6f3e
Dolzhenkov-Nikita <Dolzhenkov.na@gmail.com>
2024-10-04 18:10:40 +0500

    test():

--- src/EnvoyServiceProvider.php
+++ src/EnvoyServiceProvider.php
@@ -19,7 +19,7 @@ class EnvoyServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__ . '/Envoy.blade.php' => './Envoy.blade.php',
-            __DIR__ . '/EnvoySection/Envoy/UI/API/Routes' => './app/Containers/EnvoySection/Envoy/UI/API/Routes',
+            __DIR__ . '/EnvoySection/Envoy/UI/API/Routes/DeployHook.v1.private.php' => './app/Containers/EnvoySection/Envoy/UI/API/Routes/DeployHook.v1.private.php',
             __DIR__ . '/config/github-webhooks.php' => config_path('github-webhooks.php'),
             __DIR__ . '/database/migrations/2024_09_19_164844_create_github_webhook_calls_table.php' => database_path('migrations/2024_09_19_164844_create_github_webhook_calls_table.php'),
         ], 'bat-envoy');
